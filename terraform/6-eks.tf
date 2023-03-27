df4d3e50bd3d8f29bd6beec6f6b4345423557de7
spartzames <spartzames@gmail.com>
2023-03-27 16:16:58 +0900

    bug fix

--- terraform/6-eks.tf
+++ terraform/6-eks.tf
@@ -32,10 +32,10 @@ resource "aws_eks_cluster" "cluster" {
     public_access_cidrs = [ "0.0.0.0/0" ]
 
     subnet_ids = [
-        aws_subnet.private-ap-northeast-1a.id,
-        aws_subnet.private-ap-northeast-1b.id,
-        aws_subnet.public-ap-northeast-1a.id,
-        aws_subnet.public-ap-northeast-1b.id
+        aws_subnet.private-ap-northeast-2a.id,
+        aws_subnet.private-ap-northeast-2b.id,
+        aws_subnet.public-ap-northeast-2a.id,
+        aws_subnet.public-ap-northeast-2b.id
     ]
   }
 
