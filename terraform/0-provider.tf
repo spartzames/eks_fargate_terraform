ee8428facde38b5ba4627059fb73d6d0d93eb357
spartzames <spartzames@gmail.com>
2023-03-27 15:51:50 +0900

    init prj

---
+++ terraform/0-provider.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+variable "cluster_name" {
+  default = "demo"
+}
+
+variable "cluster_version" {
+  default = "1.22"
+}
+
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.0"
+    }
+  }
+}
