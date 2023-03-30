f1040d4784bbf7984e7c4aefeeaf021840a00b80
spartzames <spartzames@gmail.com>
2023-03-30 10:57:14 +0900

    add profile

--- terraform/7-kube-system-profile.tf
+++ terraform/7-kube-system-profile.tf
@@ -31,5 +31,54 @@ resource "aws_eks_fargate_profile" "kube-system" {
     selector {
       namespace = "kube-system"
     }
+}
 
+data "aws_eks_cluster_auth" "eks" {
+    name = aws_eks_cluster.cluster.id
+}
+
+resource "null_resource" "k8s_patcher" {
+  depends_on = [
+    aws_eks_fargate_profile.kube-system
+  ]
+
+  triggers = {
+    endpoint = aws_eks_cluster.cluster.endpoint
+    ca_crt = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+    token = data.aws_eks_cluster_auth.eks.token
+  }
+
+    provisioner "local-exec" {
+    command = <<EOH
+    cat >/tmp/ca.crt <<EOF
+    ${base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)}
+    EOF
+    kubectl \
+    --server="${aws_eks_cluster.cluster.endpoint}" \
+    --certificate_authority=/tmp/ca.crt \
+    --token="${data.aws_eks_cluster_auth.eks.token}" \
+    patch deployment coredns \
+    -n kube-system --type json \
+    -p='[{"op": "remove", "path": "/spec/template/metadata/annotations/eks.amazonaws.com~1compute-type"}]'
+    EOH
+  }
+
+  lifecycle {
+    ignore_changes = [triggers]
+  }
+}
+
+resource "aws_eks_fargate_profile" "staging" {
+  cluster_name           = aws_eks_cluster.cluster.name
+  fargate_profile_name   = "staging"
+  pod_execution_role_arn = aws_iam_role.eks-fargate-profile.arn
+
+  subnet_ids = [
+    aws_subnet.private-us-east-1a.id,
+    aws_subnet.private-us-east-1b.id
+  ]
+
+  selector {
+    namespace = "staging"
+  }
 }
