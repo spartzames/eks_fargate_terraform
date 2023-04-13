8a9170f432d5f7a2bf4d8111cfb4bf0b0d0ec78d
spartzames <spartzames@gmail.com>
2023-04-13 16:57:12 +0900

    add rm compute type ec2

---
+++ terraform/10-remove-compute-type-ec2.tf
@@ -0,0 +1,28 @@
+resource "null_resource" "k8s_patcher" {
+  depends_on = [aws_eks_fargate_profile.kube-system]
+
+  triggers = {
+    endpoint = aws_eks_cluster.cluster.endpoint
+    ca_crt   = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+    token    = data.aws_eks_cluster_auth.eks.token
+  }
+
+  provisioner "local-exec" {
+    command = <<EOH
+cat >/tmp/ca.crt <<EOF
+${base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)}
+EOF
+kubectl \
+  --server="${aws_eks_cluster.cluster.endpoint}" \
+  --certificate_authority=/tmp/ca.crt \
+  --token="${data.aws_eks_cluster_auth.eks.token}" \
+  patch deployment coredns \
+  -n kube-system --type json \
+  -p='[{"op": "remove", "path": "/spec/template/metadata/annotations/eks.amazonaws.com~1compute-type"}]'
+EOH
+  }
+
+  lifecycle {
+    ignore_changes = [triggers]
+  }
+}
