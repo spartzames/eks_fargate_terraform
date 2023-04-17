939aefe9a596e3dfafb5172e746d2325a447579f
spartzames <spartzames@gmail.com>
2023-04-17 09:28:52 +0900

    mod kubectl cmd

--- terraform/10-remove-compute-type-ec2.tf
+++ terraform/10-remove-compute-type-ec2.tf
@@ -1,36 +1,36 @@
-data "aws_eks_cluster_auth" "eks" {
-  name = aws_eks_cluster.cluster.id
-}
+# data "aws_eks_cluster_auth" "eks" {
+#   name = aws_eks_cluster.cluster.id
+# }
 
-resource "null_resource" "k8s_patcher" {
-  depends_on = [aws_eks_fargate_profile.kube-system]
+# resource "null_resource" "k8s_patcher" {
+#   depends_on = [aws_eks_fargate_profile.kube-system]
 
-  triggers = {
-    endpoint = aws_eks_cluster.cluster.endpoint
-    ca_crt   = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
-    token    = data.aws_eks_cluster_auth.eks.token
-  }
+#   triggers = {
+#     endpoint = aws_eks_cluster.cluster.endpoint
+#     ca_crt   = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+#     token    = data.aws_eks_cluster_auth.eks.token
+#   }
 
-  provisioner "local-exec" {
-    command = "curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl"
-  }
+#   provisioner "local-exec" {
+#     command = "curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl"
+#   }
 
-  provisioner "local-exec" {
-    command = <<EOH
-cat >/tmp/ca.crt <<EOF
-${base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)}
-EOF
-kubectl \
-  --server="${aws_eks_cluster.cluster.endpoint}" \
-  --certificate_authority=/tmp/ca.crt \
-  --token="${data.aws_eks_cluster_auth.eks.token}" \
-  patch deployment coredns \
-  -n kube-system --type json \
-  -p='[{"op": "remove", "path": "/spec/template/metadata/annotations/eks.amazonaws.com~1compute-type"}]'
-EOH
-  }
+#   provisioner "local-exec" {
+#     command = <<EOH
+# cat >/tmp/ca.crt <<EOF
+# ${base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)}
+# EOF
+# kubectl \
+#   --server="${aws_eks_cluster.cluster.endpoint}" \
+#   --certificate_authority=/tmp/ca.crt \
+#   --token="${data.aws_eks_cluster_auth.eks.token}" \
+#   patch deployment coredns \
+#   -n kube-system --type json \
+#   -p='[{"op": "remove", "path": "/spec/template/metadata/annotations/eks.amazonaws.com~1compute-type"}]'
+# EOH
+#   }
 
-  lifecycle {
-    ignore_changes = [triggers]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [triggers]
+#   }
+# }
