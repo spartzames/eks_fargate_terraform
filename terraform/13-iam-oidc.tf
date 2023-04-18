9fe9eaaf845b66bb0616a95e4333a82f9711cc7e
spartzames <spartzames@gmail.com>
2023-04-18 09:26:41 +0900

    fix

--- terraform/13-iam-oidc.tf
+++ terraform/13-iam-oidc.tf
@@ -4,6 +4,6 @@ data "tls_certificate" "eks" {
 
 resource "aws_iam_openid_connect_provider" "eks" {
   client_id_list = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks.certificate[0].sha1_fingerprint]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
   url = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 }
